516e179d4b1f8d978e317119cbeae2dbed834f53
botsimples <botsimplesoficial@gmail.com>
2025-11-09 04:38:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("layout", "layout");
 
 /* MongoDB */
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
