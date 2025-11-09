e742ea966a56b2539fc7f58026231f0802add57d
botsimples <botsimplesoficial@gmail.com>
2025-11-08 21:51:21 -0300

    Update Plan.js

--- models/Plan.js
+++ models/Plan.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
 
 const planSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, default: "" },
-  deliverable: { type: String, default: "" }, // link ou mensagem de entrega
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  deliverable: {
+    type: String, // Pode ser link, texto, mensagem ou arquivo
+    default: "",
+  },
 });
 
 export default mongoose.models.Plan || mongoose.model("Plan", planSchema);
