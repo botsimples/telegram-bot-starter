daf21e1cb7ea8272f067a4579d5b0ffc76522ee1
botsimples <botsimplesoficial@gmail.com>
2025-11-13 04:29:42 -0300

    Update Offer.js

--- models/Offer.js
+++ models/Offer.js
@@ -6,28 +6,41 @@ const OfferSchema = new mongoose.Schema({
     ref: "Admin",
     required: true
   },
+
   name: {
     type: String,
     required: true,
     trim: true
   },
+
   botType: {
     type: String,
     enum: ["bot_padrao", "bot_fluxo_wiin", "bot_fluxo_manychat"],
     default: "bot_padrao"
   },
+
   trackingType: {
     type: String,
     enum: ["fb_pixel", "utmify_utmify_pixel", "utmify_fb_pixel", "track_proprio"],
     default: "fb_pixel"
   },
-  botToken: String,
-  telegramUsername: String,
+
+  botToken: {
+    type: String,
+    default: null
+  },
+
+  telegramUsername: {
+    type: String,
+    default: null
+  },
+
   status: {
     type: String,
     enum: ["incompleto", "ativo", "erro"],
     default: "incompleto"
   },
+
   createdAt: {
     type: Date,
     default: Date.now
