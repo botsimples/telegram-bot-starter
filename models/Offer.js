64ecb525e0c066040db0ced5cb42d68fe85f2178
botsimples <botsimplesoficial@gmail.com>
2025-11-13 04:01:34 -0300

    Update Offer.js

--- models/Offer.js
+++ models/Offer.js
@@ -1,33 +1,36 @@
-// models/Offer.js
 const mongoose = require('mongoose');
 
 const OfferSchema = new mongoose.Schema({
+
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Admin', // ou o nome do seu model de usuário
+    ref: 'Admin',
     required: true
   },
+
   name: {
     type: String,
     required: true,
     trim: true
   },
 
-  // Tipo de bot que o seller escolheu
   botType: {
     type: String,
     enum: ['bot_padrao', 'bot_fluxo_wiin', 'bot_fluxo_manychat'],
     default: 'bot_padrao'
   },
 
-  // Tipo de traqueamento escolhido
   trackingType: {
     type: String,
-    enum: ['fb_pixel', 'utmify_utmify_pixel', 'utmify_fb_pixel', 'track_proprio'],
+    enum: [
+      'fb_pixel',
+      'utmify_utmify_pixel',
+      'utmify_fb_pixel',
+      'track_proprio'
+    ],
     default: 'fb_pixel'
   },
 
-  // Token do bot Telegram (opcional na criação)
   botToken: {
     type: String,
     default: null
@@ -48,6 +51,7 @@ const OfferSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+  
 });
 
 module.exports = mongoose.model('Offer', OfferSchema);
