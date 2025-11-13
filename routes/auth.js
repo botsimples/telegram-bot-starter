b47b1fa1a53a1f8ddec651b5c9bd58a5d6301924
botsimples <botsimplesoficial@gmail.com>
2025-11-13 04:38:54 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Admin = require("../models/admin");
+const Admin = require("../models/Admin");
 const bcrypt = require("bcrypt");
 
 // ===== LOGIN =====
